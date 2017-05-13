e5638c969f5aca069ead609c92a3f0f1220862ec
hippiewho <hippiewho@yahoo.com>
2017-05-12 20:24:48 -0700

    Fixed, Cleaned, Commented

--- src/main/driver.java
+++ src/main/driver.java
@@ -6,31 +6,31 @@ public class driver {
 
 	public static void main(String[] args) {
 		DigitalSignature ds;
+		Scanner sc = new Scanner(System.in);
 
 		while(true) {
-			Scanner sc = new Scanner(System.in);
 			printMenu();
 			int selection = sc.nextInt();
-			
 			switch(selection){
 				case 1:
 					KeyGen kg = new KeyGen();
 					kg.createKeys();
 					break;
 				case 2:
-					ds = new DigitalSignature("test.txt");
+					ds = new DigitalSignature(sc);
 					ds.prepareFile();
 					break;
 				case 3:
-					ds = new DigitalSignature("test.txt");
+					ds = new DigitalSignature(sc);
 					ds.readFile();
 					break;
 				case 0:
 					sc.close();
 					return;
 				default:
-					sc.close();
+					return;
 			}
+			
 		}
 		
 	}
